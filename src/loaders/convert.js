7bfcb93f48fc91f5ade5603f4c80f4b509c64b75
KensoDev <avi@kensodev.com>
2018-05-03 16:26:26 -0700

    Load from secrets manager

--- src/loaders/convert.js
+++ src/loaders/convert.js
@@ -14,6 +14,7 @@ export function dotify(envvar) {
  */
 export function convertBoolean(value) {
     const stringValue = value.toString().toLowerCase();
+    console.log(stringValue);
     if (stringValue === '0' || stringValue === 'false') {
         return false;
     }
--- src/loaders/secretsManager.js
+++ src/loaders/secretsManager.js
@@ -1,3 +1,66 @@
+import AWS from 'aws-sdk';
+import { merge } from 'lodash';
+
+import { CREDSTASH_PREFIX } from '../constants';
+import { convert } from './convert';
+
+
+export function getClient() {
+    return new AWS.SecretsManager();
+}
+
+function convertAllKeys(obj) {
+    if (typeof obj !== 'object') return convert(obj);
+
+    const newObj = {};
+
+    Object.keys(obj).forEach((name) => {
+        newObj[name] = convertAllKeys(obj[name]);
+    });
+
+    return newObj;
+}
 
 export default async function loadFromSecretsManager(metadata) {
+    const version = process.env[`${CREDSTASH_PREFIX}_CONFIG_VERSION`];
+    const environment = process.env[`${CREDSTASH_PREFIX}_ENVIRONMENT`];
+
+    if (!version) {
+        const message = `Must define environment variable: ${CREDSTASH_PREFIX}_CONFIG_VERSION`;
+
+        if (!metadata.debug) {
+            throw new Error(message);
+        }
+
+        // allow bypassing credstash during debug
+        return {};
+    }
+
+    if (!environment) {
+        const message = `Must define environment variable: ${CREDSTASH_PREFIX}_ENVIRONMENT`;
+
+        if (!metadata.debug) {
+            throw new Error(message);
+        }
+
+        // allow bypassing credstash during debug
+        return {};
+    }
+
+    const secretId = `secrets/${environment}/${metadata.name}`;
+    const client = getClient();
+
+    const secrets = await new Promise((resolve, reject) => {
+        client.getSecretValue({
+            SecretId: secretId,
+            VersionStage: version,
+        }, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(convertAllKeys(JSON.parse(data.SecretString)));
+        });
+    });
+
+    return secrets;
 }
