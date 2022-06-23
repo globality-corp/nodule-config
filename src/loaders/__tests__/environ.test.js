6bf6f0fb535bb8b6f4ebc4e9c146bb0431c33813
Alexander Roe <70572792+alex-roe-globality@users.noreply.github.com>
2022-06-23 14:55:35 +0100

    GLOB-61900 Add Typescript Support (#60)
    * GLOB-61900
    
    - Enable TypeScript
    - Use gi-eslint
    - Fix test running
    - Bump node version
    - Migrate injector to TS

--- src/loaders/__tests__/environ.test.js
+++ src/loaders/__tests__/environ.test.js
@@ -1,41 +1,38 @@
-import loadFromEnvironment from '../environ';
-import Metadata from '../../metadata';
+import Metadata from "../../metadata";
+import loadFromEnvironment from "../environ";
 
+describe("loadFromEnvironment", () => {
+  beforeAll(() => {
+    process.env.TEST__GROUP__VARIABLE = "X";
+    process.env.TEST__VARIABLE = "Y";
+    process.env.TEST__BOOL_VARIABLE_FALSE = "0";
+    process.env.TEST__BOOL_VARIABLE_TRUE = "1";
+    process.env.TEST__BOOL_VARIABLE_FALSE_LITERAL = "False";
+    process.env.TEST__BOOL_VARIABLE_TRUE_LITERAL = "True";
+    process.env.TEST__BOOL_VARIABLE_FALSE_LITERAL_D = "false";
+    process.env.TEST__BOOL_VARIABLE_TRUE_LITERAL_D = "true";
+  });
 
-describe('loadFromEnvironment', () => {
+  it("should have the correct configuration", () => {
+    const config = loadFromEnvironment(new Metadata({ name: "test" }));
 
-    beforeAll(() => {
-        process.env.TEST__GROUP__VARIABLE = 'X';
-        process.env.TEST__VARIABLE = 'Y';
-        process.env.TEST__BOOL_VARIABLE_FALSE = '0';
-        process.env.TEST__BOOL_VARIABLE_TRUE = '1';
-        process.env.TEST__BOOL_VARIABLE_FALSE_LITERAL = 'False';
-        process.env.TEST__BOOL_VARIABLE_TRUE_LITERAL = 'True';
-        process.env.TEST__BOOL_VARIABLE_FALSE_LITERAL_D = 'false';
-        process.env.TEST__BOOL_VARIABLE_TRUE_LITERAL_D = 'true';
+    expect(config).toEqual({
+      boolVariableFalse: false,
+      boolVariableTrue: true,
+      boolVariableFalseLiteral: false,
+      boolVariableTrueLiteral: true,
+      boolVariableFalseLiteralD: false,
+      boolVariableTrueLiteralD: true,
+      group: {
+        variable: "X",
+      },
+      variable: "Y",
     });
+  });
 
-    it('should have the correct configuration', () => {
-        const config = loadFromEnvironment(new Metadata({ name: 'test' }));
+  it("should filter out non-matching variables", () => {
+    const config = loadFromEnvironment(new Metadata({ name: "other" }));
 
-        expect(config).toEqual({
-            boolVariableFalse: false,
-            boolVariableTrue: true,
-            boolVariableFalseLiteral: false,
-            boolVariableTrueLiteral: true,
-            boolVariableFalseLiteralD: false,
-            boolVariableTrueLiteralD: true,
-            group: {
-                variable: 'X',
-            },
-            variable: 'Y',
-        });
-    });
-
-    it('should filter out non-matching variables', () => {
-        const config = loadFromEnvironment(new Metadata({ name: 'other' }));
-
-        expect(config).toEqual({
-        });
-    });
+    expect(config).toEqual({});
+  });
 });
