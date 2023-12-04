ae3792aa8d1db3e879462de5217cefe6fd5b7d0b
William Parsons-Heins <william.parsons-heins@globality.com>
2023-12-04 14:12:15 +0000

    convert.js to typescript

--- src/loaders/convert.ts
+++ src/loaders/convert.ts
@@ -2,15 +2,17 @@ import { camelCase, set } from "lodash";
 
 import { SEPARATOR } from "../constants";
 
-/* Transform environment variables to dotted keys.
- */
-export function dotify(envvar) {
+/**
+ * Transform environment variables to dotted keys.
+ * */
+export function dotify(envvar: string) {
   return envvar.split(SEPARATOR).map(camelCase).join(".");
 }
 
-/* Convert a boolean value to a boolean type.
- */
-export function convertBoolean(value) {
+/**
+ * Convert a boolean value to a boolean type.
+ * */
+export function convertBoolean(value: any) {
   const stringValue = value.toString().toLowerCase();
   if (stringValue === "0" || stringValue === "false") {
     return false;
@@ -21,9 +23,10 @@ export function convertBoolean(value) {
   return null;
 }
 
-/* Convert values to appropriate types.
- */
-export function convert(value) {
+/**
+ * Convert values to appropriate types.
+ * */
+export function convert(value: any) {
   // XXX conversion should be configurable
   const converted = convertBoolean(value);
   if (converted !== null) {
@@ -32,7 +35,7 @@ export function convert(value) {
   return value;
 }
 
-export default function toObject(key, value) {
+export default function toObject(key: string, value: any) {
   const config = {};
   set(config, dotify(key), convert(value));
   return config;
