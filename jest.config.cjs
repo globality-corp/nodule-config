3ee6a9c08e66d57b781c7ca1b4eab1b5e26b7d0f
William Parsons-Heins <william.parsons-heins@globality.com>
2024-09-17 14:30:37 +0100

    update jest config to work with esm

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,15 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const { createDefaultEsmPreset } = require('ts-jest')
+
+const defaultEsmPreset = createDefaultEsmPreset()
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest/presets/js-with-ts',
+    preset: 'ts-jest/presets/js-with-ts-esm',
     testEnvironment: 'node',
     verbose: true,
     roots: ['<rootDir>/src'],
+    ...defaultEsmPreset,
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    }
 };
